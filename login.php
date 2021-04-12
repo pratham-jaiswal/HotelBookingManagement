280fae4ca92ad9dd034ddbf0ba385862dc37961c
pratham <72384535+pratham@users.noreply.github.com>
2021-04-12 21:34:59 +0530

    login page - add forgot password

--- login.php
+++ login.php
@@ -4,7 +4,7 @@ session_start();
 
 //Check if user is already logged in
 if(isset($_SESSION['username'])){
-    header("location: welcome.php");
+    header("location: accountInfo.php");
     exit();
 }
 
@@ -24,16 +24,15 @@ if($_SERVER['REQUEST_METHOD']=="POST"){
 }
 
 if(empty($err)){
-    $sql = "SELECT id, username, password FROM users WHERE username = ?";
+    $sql = "SELECT id, fname, lname, email, username, password FROM loginform WHERE username = ?";
     $stmt = mysqli_prepare($conn, $sql);
     mysqli_stmt_bind_param($stmt, "s", $param_username);
     $param_username = $username;
-
     //Try to execute this statement
     if(mysqli_stmt_execute($stmt)){
         mysqli_stmt_store_result($stmt);
         if(mysqli_stmt_num_rows($stmt) == 1){
-            mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
+            mysqli_stmt_bind_result($stmt, $id, $fname, $lname, $email, $username, $hashed_password);
             if(mysqli_stmt_fetch($stmt)){
                 if(password_verify($password, $hashed_password)){
                     //Password is correct. Allow user to login
@@ -42,8 +41,8 @@ if(empty($err)){
                     $_SESSION["id"] = $id;
                     $_SESSION["loggedin"] = true;
 
-                    //Redirect the user to the welcome page
-                    header("location: login.php");
+                    //Redirect the user to the accountInfo page
+                    header("location: accountInfo.php");
                 }
             }
         }
@@ -62,13 +61,13 @@ if(empty($err)){
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-
-    <title>PHP Login System</title>
+    <link rel="stylesheet" href="style.css" />
+    <title>Hotel Booking Management</title>
   </head>
   <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">PHP Login System</a>
+            <a class="navbar-brand" href="#">Hotel Booking Management</a>
             <ul class="navbar-nav">
                 <li class="nav-item">
                     <a class="nav-link" href="register.php">Register <i class="fa fa-user-plus" aria-hidden="true"></i></a>
@@ -88,7 +87,7 @@ if(empty($err)){
     <hr>
     <form action="" method="post">
         <div class="mb-3">
-        <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+        <label for="inputEmail3" class="col-sm-2 col-form-label">Username</label>
             <div class="col-sm-10">
             <input type="text" class="form-control" name="username" id="inputEmail3">
         </div>
