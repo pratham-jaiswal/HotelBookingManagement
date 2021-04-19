8b77826f38c77a243da58890f2719a81370b1226
pratham <72384535+pratham@users.noreply.github.com>
2021-04-19 10:50:59 +0530

    updated database name

--- config.php
+++ config.php
@@ -7,7 +7,7 @@ user "root" and password ""
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'login');
+define('DB_NAME', 'hotelbookingmanagement');
 
 //Trying to conne ct to database
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
