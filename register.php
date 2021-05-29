aa4bb060fcf2400b6ac93ebd5e9072e4fdfb128a
pratham <72384535+pratham@users.noreply.github.com>
2021-05-29 21:11:09 +0530

    8 character password needed

--- register.php
+++ register.php
@@ -123,8 +123,8 @@ if($_SERVER['REQUEST_METHOD']=="POST"){
     if(empty(trim($_POST['password']))){
         $password_err = "Password cannot be blank";
     }
-    elseif(strlen(trim($_POST['password'])) < 5){
-        $password_err = "Password cannot be less than 5 characters";
+    elseif(strlen(trim($_POST['password'])) < 8){
+        $password_err = "Password cannot be less than 8 characters";
     }
     else{
         $password = trim($_POST['password']);
